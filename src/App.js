a6966dfca27d2f9d0c6020a90973f70ba1d60278
Mohamed Azmeer <azmeer.crescent@gmail.com>
2019-12-13 19:23:12 +0530

    Readme updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
         <h3>React Workshop</h3>
         <code>11th Nov 2019</code>
-        <p>Using <a href="https://reactjs.org/docs/hooks-state.html" target="_blank">useState</a> hook for global state management.</p>
+        <p>Using <a href="https://reactjs.org/docs/hooks-state.html" target="_blank">useState</a> hook for local state management.</p>
         <p><a href="https://reacttraining.com/react-router/web/guides/quick-start" target="_blank">ReactRouter</a> to navigate from one page together.</p>
         <p><a href="https://react-redux.js.org/next/api/hooks" target="_blank">Redux Hooks</a> for global state management</p>
         <br />
